6239026ff81ed88871ccdf0763b1ff1972a89284
yeenathan <yeenathan21@gmail.com>
2024-03-21 14:45:27 -0700

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.frame}>
+        <header>
+          header
+        </header>
+        <main className={styles.main}>
+          <h1>StudiUs</h1>
+        </main>
+        <footer>
+          footer
+        </footer>
+      </div>
+    </>
+  );
+}
