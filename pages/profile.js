025e7023a62615ec62d21dc3421d26e4914a970a
JeromeGache <jgache1@my.bcit.ca>
2024-03-28 11:41:45 -0700

    profile

---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default function profile() {
+    return (
+        <div>
+            <Header />
+            <div>profile</div>
+            <Footer />
+        </div>
+
+    )
+}
