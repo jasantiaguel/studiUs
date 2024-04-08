870cafe58989302f99116b8e4d300143e0336422
yeenathan <yeenathan21@gmail.com>
2024-04-08 11:52:24 -0700

    implemented question descriptions

--- data/quiz.js
+++ data/quiz.js
@@ -4,43 +4,43 @@
 const FEYNMAN = "Feynman";
 const LEITNER = "Leitner";
 const QR = "SQ3R/PQ4R";
-const RETRIEVAL = "retrieval";
+const RETRIEVAL = "Retrieval";
 
 export const questions = [
     {
         question: "Question 1",
         answers: [
-            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1},
-            {text: "Feynman +2", method: [FEYNMAN], value: 2},
-            {text: "Leitner +2", method: [LEITNER], value: 2},
-            {text: "Retrieval +2", method: [RETRIEVAL], value: 2}
+            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1, description: "This is the description for Feynman, Leitner +1 for question 1."},
+            {text: "Feynman +2", method: [FEYNMAN], value: 2, description: "This is the description for Feynman +2 for question 1."},
+            {text: "Leitner +2", method: [LEITNER], value: 2, description: "This is the description for Leitner +2 for question 1."},
+            {text: "Retrieval +2", method: [RETRIEVAL], value: 2, description: "This is the description for Retrieval +2 for question 1."}
         ]
     },
     {
         question: "Question 2",
         answers: [
-            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1},
-            {text: "Feynman +2", method: [FEYNMAN], value: 2},
-            {text: "Leitner +2", method: [LEITNER], value: 2},
-            {text: "Retrieval +2", method: [RETRIEVAL], value: 2}
+            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1, description: "This is the description for Feynman, Leitner +1 for question 2."},
+            {text: "Feynman +2", method: [FEYNMAN], value: 2, description: "This is the description for Feynman +2 for question 2."},
+            {text: "Leitner +2", method: [LEITNER], value: 2, description: "This is the description for Leitner +2 for question 2."},
+            {text: "Retrieval +2", method: [RETRIEVAL], value: 2, description: "This is the description for Retrieval +2 for question 2."}
         ]
     },
     {
         question: "Question 3",
         answers: [
-            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1},
-            {text: "Feynman +2", method: [FEYNMAN], value: 2},
-            {text: "Leitner +2", method: [LEITNER], value: 2},
-            {text: "Retrieval +2", method: [RETRIEVAL], value: 2}
+            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1, description: "This is the description for Feynman, Leitner +1 for question 3."},
+            {text: "Feynman +2", method: [FEYNMAN], value: 2, description: "This is the description for Feynman +2 for question 3."},
+            {text: "Leitner +2", method: [LEITNER], value: 2, description: "This is the description for Leitner +2 for question 3."},
+            {text: "Retrieval +2", method: [RETRIEVAL], value: 2, description: "This is the description for Retrieval +2 for question 3."}
         ]
     },
     {
         question: "Question 4",
         answers: [
-            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1},
-            {text: "Feynman +2", method: [FEYNMAN], value: 2},
-            {text: "Leitner +2", method: [LEITNER], value: 2},
-            {text: "Retrieval +2", method: [RETRIEVAL], value: 2}
+            {text: "Feynman, Leitner +1", method: [FEYNMAN, LEITNER], value: 1, description: "This is the description for Feynman, Leitner +1 for question 4."},
+            {text: "Feynman +2", method: [FEYNMAN], value: 2, description: "This is the description for Feynman +2 for question 4."},
+            {text: "Leitner +2", method: [LEITNER], value: 2, description: "This is the description for Leitner +2 for question 4."},
+            {text: "Retrieval +2", method: [RETRIEVAL], value: 2, description: "This is the description for Retrieval +2 for question 4."}
         ]
     }
 ]
--- pages/quiz/questions.js
+++ pages/quiz/questions.js
@@ -17,33 +17,42 @@ export default function Questions() {
     const [leitner, setLeitner] = useState(0);
     const [retrieval, setRetrieval] = useState(0);
     const [showSubmit, setShowSubmit] = useState(false);
+    const [questionSelected, setQuestionSelected] = useState(false);
+    const [answerSelected, setAnswerSelected] = useState();
     const router = useRouter();
 
     function determineMethod() {
-        if (feynman >= qr && feynman >= leitner >= feynman >= retrieval) return "Feynman"; // in case of tie, first prio
-        if (qr >= leitner && qr >= retrieval && pq > feynman) return "SQ3R/PQ4R"; // second prio
-        if (leitner > qr && leitner > feynman && leitner >= retrieval) return "Leitner"; // third prio
-        if (retrieval > qr && retrieval > leitner && retrieval > feynman) return "Retrieval"; //fourth prio
+        let result = [feynman, qr, leitner, retrieval];
+        let max = Math.max(...result);
+        if (feynman == max) return "Feynman";
+        if (qr == max) return "SQ3R/PQ4R";
+        if (leitner == max) return "Leitner";
+        if (retrieval == max) return "Retrieval";
     }
 
     //processes data coming back from Question component (go to component for more details)
     const returnData = (data) => { //`data` is the object returned by Question
-        if (data.method.includes("Feynman")) setFeynman(feynman + data.value);
+        setAnswerSelected(data);
+        if (data.method.includes("Feynman"))setFeynman(feynman + data.value);
         if (data.method.includes("Leitner")) setLeitner(leitner + data.value);
         if (data.method.includes("SQ3R/PQ4R")) setQR(qr + data.value);
         if (data.method.includes("Retrieval")) setRetrieval(retrieval + data.value);
     }
 
-    //dictates question click behaviour
-    const handleClick = () => {
-        if (currentQuestion < questions.length-1) { //needs this condition so that it doesn't overflow
+    const handleNext = () => {
+        setQuestionSelected(false);
+        if (currentQuestion < questions.length-1) {
             setCurrentQuestion(currentQuestion+1);
-        }
-        else {
-            setShowSubmit(true);
         }
     }
 
+    const handleQuestion = () => {
+        setQuestionSelected(true);
+        if (currentQuestion == questions.length-1) {
+            setShowSubmit(true);
+        }
+    }
+    console.log(determineMethod());
     //back button
     const handleBack = () => {
         if (currentQuestion > 0) {
@@ -53,21 +62,27 @@ export default function Questions() {
             router.push("./home");
         }
     }
-
     return(
         <div className="frame">
             <Header/>
-            <div className={styles.main}>
-                <h1>{questions[currentQuestion].question}</h1>
-                {questions[currentQuestion].answers.map((answer) => {
-                    return <Question onclick={handleClick} answerData={answer} returnData={returnData}/>
-                })}
-            </div>
+            <h1>{questions[currentQuestion].question}</h1>
+                <div className={styles.main}>
+                {   !questionSelected ?
+                    questions[currentQuestion].answers.map((answer) => {
+                        return <Question onclick={handleQuestion} answerData={answer} returnData={returnData}/>
+                    }) : 
+                    <>
+                        <p>{answerSelected.description}</p>
+                        {
+                            showSubmit ?
+                            <Link href={{pathname: "./results", query: {method: determineMethod()}}}><Button text="Finish"/></Link> :
+                            <Button onclick={handleNext} text="next"/>
+                        }
+                    </>
+                }
+                </div>
             <div className={styles.buttons}>
                 <Button onclick={handleBack} text="back"/>
-                {   showSubmit ?
-                    <Link href={{pathname: "./results", query: {method: determineMethod()}}}><Button text="Finish"/></Link> : null
-                }
             </div>
             <Footer/>
         </div>
