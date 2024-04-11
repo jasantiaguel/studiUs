727d486c8c557aac3f6a44c348825a7a35ef4f32
jasantiaguel <jsantiaguel2@my.bcit.ca>
2024-04-10 20:12:38 -0700

    merging

--- pages/quiz/questions.js
+++ pages/quiz/questions.js
@@ -122,9 +122,7 @@ export default function Questions() {
                         {!answerSubmitted ?
                             // Screen 1 buttons
                             <>
-                                <button className={styles.buttonOverride} onClick={handleBack}>
-                                    <Image onclick={handleBack} src='/images/back.arrow.svg' width={41} height={48}/>
-                                </button>
+                                <Button onclick={handleBack}><Image src='/images/back.arrow.svg' width={41} height={48}/></Button>
                                 {
                                     answerIsSelected &&
                                     <Button onclick={handleNext} text="Next" /> // "Submits selected answer"
@@ -136,7 +134,7 @@ export default function Questions() {
                                 <Button onclick={() => setPopup(true)} text="Learn More" width="179px" />
                                 {   // Handles which button to show on last question
                                     showSubmit ?
-                                        <Link href={{ pathname: "./results", query: { results: JSON.stringify(getResults()) } }}><Button text="Finish" width="179px" /></Link> :
+                                        <Link href={{ pathname: "./results", query: { results: JSON.stringify(getResults()) } }}><Button text="Finish" width="179px"/></Link> :
                                         <Button onclick={handleNextQuestion} text="Next Question" width="179px" />
                                 }
                             </>
