e45b5e6a4e2a3ffe42b7b7b2506e5b411cfc8897
JeromeGache <jgache1@my.bcit.ca>
2024-04-18 12:42:24 -0700

    adding icons

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,14 +1,18 @@
 import NavTab from "@/components/NavTab"
 import styles from "./Footer.module.css"
 
+
+
 export default function Footer() {
+const icon ={}
+
     return(
         <div className={styles.footer}>
-            <NavTab image="/" activeImage="/" path="/"/>
-            <NavTab image="/" activeImage="/" path="/search"/>
-            <NavTab image="/" activeImage="/" path="/groups"/>
-            <NavTab image="/" activeImage="/" path="/quiz/home"/>
-            <NavTab image="/" activeImage="/" path="/profile"/>
+            <NavTab image="/images/icon.home.svg" activeImage="/images/icon.home-selected.svg" path="/"/>
+            <NavTab image="/images/icon.search.svg" activeImage="/images/icon.search-selected.svg" path="/search"/>
+            <NavTab image="/images/icon.groups.svg" activeImage="/images/icon.groups-selected.svg" path="/groups"/>
+            <NavTab image="/images/icon.quiz.svg" activeImage="/images/icon.quiz-selected.svg" path="/quiz/home"/>
+            <NavTab image="/images/icon.profile.svg" activeImage="/images/icon.profile-selected.svg" path="/profile"/>
         </div>
     )
 }
