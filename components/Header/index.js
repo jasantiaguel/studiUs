5d3deaacb1b66e3bf69315d301ac6c3a2da3a72e
Jon Santiaguel <jsantiaguel2@my.bcit.ca>
2024-04-30 13:16:29 -0700

    fixing header font failure on refresh + installing framer motion

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,11 +4,7 @@ import Menu from "@/components/Menu";
 export default function Header({name, bgColor}) {
     return(
         <div style={{backgroundColor: bgColor}} className={styles.header}>
-            <h2 style={{
-              fontSize: "var(--largerer-title", 
-              fontWeight: "var(--font-weight-bold)",
-              margin: "0px",
-            }}>
+            <h2 className={styles.headerFont}>
               {name}
             </h2>
             <Menu/>
