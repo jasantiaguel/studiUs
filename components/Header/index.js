9695515ccdde7f65333534928f9e2d9a65a43d5c
jasantiaguel <jsantiaguel2@my.bcit.ca>
2024-05-08 11:52:24 -0700

    adding backBtn functionality to Header comp

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,26 @@
 import styles from "./Header.module.css";
 import Menu from "@/components/Menu";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
-export default function Header({name, bgColor}) {
+export default function Header({ name , bgColor , backBtn }) {
+    const router = useRouter();
+
     return(
         <div style={{backgroundColor: bgColor}} className={styles.header}>
-            <h2 className={styles.headerFont}>
-                {name}
-            </h2>
+            <div className={styles.titleContainer}>
+                {backBtn && (
+                    <Image 
+                        src='/images/icons/icon.back-arrow.svg' 
+                        width={44} 
+                        height={41} 
+                        onClick={() => router.back()}
+                    />
+                )}
+                <h2 className={styles.headerFont}>
+                    {name}
+                </h2>
+            </div>
             <Menu/>
         </div>
     )
