6373f6cac796e3cebb49fec9a9f00d6e76b67c7a
Nathan <yeenathan21@gmail.com>
2024-05-11 16:44:00 -0700

    snackbar

--- pages/groups.js
+++ pages/groups.js
@@ -5,21 +5,26 @@ import HeadArea from "@/components/HeadArea";
 import GroupCard from "@/components/GroupCard";
 import Banner from "@/components/Banner";
 import { groups } from "@/data/groups";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "@/components/Button";
 import CreateOverlay from "@/components/CreateOverlay";
 import Image from "next/image";
-import PopupButton from "@/components/PopupButton";
 
 export default function Groups() {  
   const [groupData, setGroupData] = useState(groups);
   const [showCreate, setShowCreate] = useState(false);
   const [groupToRemove, setGroupToRemove] = useState();
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [snackbarText, setSnackbarText] = useState("");
 
   const newGroup = (data) => {
     let a = [...groupData];
     a.unshift(data);
     setGroupData(a);
+    setSnackbarText(
+      <p>Created group: {data.title}</p>
+    )
+    displaySnackbar();
   }
 
   const returnGroup = (data) => {
@@ -29,6 +34,15 @@ export default function Groups() {
   const removeGroup = () => {
     let a = [...groupData];
     setGroupData(a.filter(e => e !== groupToRemove));
+    setSnackbarText(
+      <p>Left group: {groupToRemove.title}</p>
+    )
+    displaySnackbar();
+  }
+
+  function displaySnackbar() {
+    setShowSnackbar(true);
+    setTimeout(() => setShowSnackbar(false), 2000);
   }
 
   return(
@@ -64,8 +78,13 @@ export default function Groups() {
           
           {/* This is a filler section to make the bottom of page not cut off */}
           <section className={styles.bottomFiller} style={{margin: '196px 0'}}>
-            <p> </p>
           </section>
+          {
+            showSnackbar &&
+            <div className={styles.snackbar}>
+              {snackbarText}
+            </div>
+          }
           <div className={styles.createButton}>
             <Button 
               text="+ Create Group" 
